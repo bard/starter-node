1c5c9f3bfb298430f26353449b06a007bb83d0ad
Massimiliano Mirra <bard@hyperstruct.net>
2020-08-14 14:06:56 +0100

    allow ignoring (and un-ignoring) paths for babel transpilation

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,13 @@ module.exports = {
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
   ],
+  // babel-node will normally not take the `ignore` field of
+  // babel.config.js into account. See:
+  // https://github.com/babel/babel/issues/8802
+  // To work around it, invoke babel-node `--ignore '[]'` with
+  // and provide ignore there. Additionally, nodemon won't
+  // pass the `--ignore '[]'` correctly to babel-node. To work
+  // around it, wrap babel-node invocation in a package.json
+  // script (`start:nowatch`) and run that via nodemon.
+  ignore: [/node_modules/],
 }
